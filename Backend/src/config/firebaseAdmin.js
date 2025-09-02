66e321f4823bdbeb916b287070b1d8e3a30086fd
Sasi <sasikiran.tt3011@gmail.com>
2025-09-03 00:20:24 +0530

    backend change

--- Backend/src/config/firebaseAdmin.js
+++ Backend/src/config/firebaseAdmin.js
@@ -1,6 +1,11 @@
 // backend/src/config/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json"); // path to downloaded JSON
+
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
